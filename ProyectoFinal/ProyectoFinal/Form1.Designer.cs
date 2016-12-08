a16d0edb29b5ac3628e59384ee9c1cb5813d452e
Victor <sparkong02@gmail.com>
2016-12-08 13:39:36 -0600

    Pez Globo
    Lanza Picos
    Si muere Cambia de color
    Gira para mirar al player
    Los picos del trasero giran cuando el gira
    Una vez muerto sube a Y = 0
    si esta en Y = 0, el pez no se mueve en la "ola"
    Falta sincronizar las animaciones

--- ProyectoFinal/ProyectoFinal/Form1.Designer.cs
+++ ProyectoFinal/ProyectoFinal/Form1.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(277, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(251, 66);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 1;
+            this.btnEnter.Text = "Entrar";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 261);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnEnter;
     }
 }
 
--- ProyectoFinal/ProyectoFinal/Form1.cs
+++ ProyectoFinal/ProyectoFinal/Form1.cs
@@ -10,11 +10,23 @@ using System.Windows.Forms;
 
 namespace ProyectoFinal
 {
+    class Nodo
+    {
+        public string info;
+        public List<String> HijosInfo;
+        public Nodo izq, der, padre;
+    }
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
